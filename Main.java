6deb4f2f9a8af434e3d732f9e50bb0d0622440b6
monjo003 <monjo003@morris.umn.edu>
2018-09-26 20:12:35 -0500

    Added user input for bin capacity, number of elements and elements. Put in bins function works now but not as we would like eventually. Removed approximate for now.

--- Main.java
+++ Main.java
@@ -1,57 +1,63 @@
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
 
-    // TODO Put elements into bins of a fixed size - B
     // TODO Return possible bin outputs
     // TODO Create approximate algorithm for bins
 
     // Main
     public static void main(String[] args) {
-        int[] elements = {12, 4, 8, 15, 9, 3, 1, 10};
-        int binCapacity = 20;
+
+        // Declare size of elements in order to get bin capacity 'B'
+        Scanner sc = new Scanner (System.in);
+
+        // Optimal would be bin capacity = size of an element with only one element
+        System.out.println("What is the bin capacity?");
+        int binCapacity = sc.nextInt();
+        System.out.println("How many elements?");
+        int[] elements = new int[sc.nextInt()];
+        System.out.println("Enter size of each element. Please give " + elements.length + " elements.");
+        for (int i = 0; i < elements.length; i++){
+            elements[i] = sc.nextInt();
+        }
+        int remainderCapacity = 10000;
+        int[] remainder = new int[elements.length];
+        int[] bin1 = new int[elements.length];
+        int[] bin2 = new int[elements.length];
+        int[] bin3 = new int[elements.length];
 
         // Put elements into bins
-        putInBins(elements, binCapacity);
+        putInBins(bin1, elements, binCapacity);
+        putInBins(bin2, elements, binCapacity);
+        putInBins(bin3, elements, binCapacity);
+        putInBins(remainder, elements, remainderCapacity);
+
         // Output list of elements in each bin
-        System.out.println(Arrays.toString(elements));
+        System.out.println("This is bin1: " + Arrays.toString(bin1));
+        System.out.println("This is bin2: " + Arrays.toString(bin2));
+        System.out.println("This is bin3: " + Arrays.toString(bin3));
+        System.out.println("This is the remainder: " + Arrays.toString(remainder));
         // Output list of unpacked items
         // Output amount of unused space
     }
 
-    // Method for puting elements of a given array into 3 separate bins
-    public static void putInBins(int[] elements, int binCapacity){
-        int bin1[] = new int [elements.length];
-        int bin2[] = new int [elements.length];
-        int bin3[] = new int [elements.length];
+    // Method for putting elements of a given array into 3 separate bins
+    public static void putInBins(int[] bin, int[] elements, int binCapacity){
 
         // Put elements into bin until we hit 20 or as close as possible
-        while (binSum(bin1, bin1.length) <= binCapacity){
-            for (int i = 0; i < elements.length; i++){
-                elements[i] = bin1[i];
-                // Remove used elements from array of elements
-                //elements[i] = 0;
-            }
-        }
-        // Put elements into bin until we hit 20 or as close as possible
-        while (binSum(bin2, bin2.length) <= binCapacity){
-            for (int i = 0; i < elements.length; i++){
-                elements[i] = bin2[i];
-                // Remove used elements from array of elements
-                //elements[i] = 0;
-            }
-        }
-        // Put elements into bin until we hit 20 or as close as possible
-        while (binSum(bin3, bin3.length) <= binCapacity){
-            for (int i = 0; i < elements.length; i++){
-                elements[i] = bin3[i];
-                // Remove used elements from array of elements
-                //elements[i] = 0;
+        int j = 0;
+
+        for (int i = 0; i < elements.length; i++) {
+            if (binSum(bin, bin.length) <= binCapacity && elements[i]+binSum(bin, bin.length) <= binCapacity){
+                bin[j] = elements[i];
+                j++;
+                elements[i]=0;
             }
         }
     }
 
-    // Find the sum of elements in a bin - used to not overfill bin
+    // Find the sum of elements in a bin - used to not overfill bin - This works!
     public static int binSum(int arr[], int n){
         int sum = 0;
 
@@ -59,11 +65,6 @@ public class Main {
             sum += arr[i];
         }
         return sum;
-    }
-
-    // Approximate the different possible bin combos
-    public static void approximate(){
-
     }
 }
 
